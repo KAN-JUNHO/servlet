5fc4184f1127420e2e90e21b698a7d25c3e87f02
junho <junho6667@gmail.com>
2021-07-20 17:24:05 +0900

    2021/07/20

--- src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface Controllerv2 {
+public interface ControllerV2 {
     MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
 }
