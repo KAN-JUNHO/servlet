40dd05709b8328549584a8965103ee044e656c79
junho <junho6667@gmail.com>
2021-07-21 18:18:51 +0900

    2021/07/21

--- src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -6,8 +6,9 @@ import java.util.Map;
 
 public class MemberFormControllerV4 implements ControllerV4 {
 
+
     @Override
     public String process(Map<String, String> paramMap, Map<String, Object> model) {
-        return "new-from";
+        return "new-form";
     }
 }
